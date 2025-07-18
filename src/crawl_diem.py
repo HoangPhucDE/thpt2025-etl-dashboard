0608686373c5271e05cb73b5495cce0c3a65f67c
Hoàng Phúc <phuchn0305@gmail.com>
2025-07-18 15:51:54 +0700

    feat/Hiện điểm thi của 100 thí sinh

--- src/crawl_diem.py
+++ src/crawl_diem.py
@@ -3,8 +3,6 @@ import requests
 def tracudiem (sbd: str, nam: int = 2025):
     url = "https://s6.tuoitre.vn/api/diem-thi-thpt.htm"
 
-    # sbd = "01016799"
-
     params = {
         "sbd": sbd,
         "year": nam
@@ -16,7 +14,33 @@ def tracudiem (sbd: str, nam: int = 2025):
 
     res = requests.get(url, params = params, headers = headers)
 
-    print('Status code: ', res.status_code)
-    print("Kết quả: ", res.text)
+    if res.status_code == 200:
+        try:
+            data = res.json()
+            diem_thi = data["data"] [0] if data["data"] else {}
+            
+            output = {
+                "SBD": diem_thi.get("SBD"),
+                "TONGDIEM": diem_thi.get("TONGDIEM")
+            }
 
-tracudiem("01016799")
+            mon_thi = ["TOAN", "VAN", "NGOAI_NGU", "SU", "DIA", "GDKT_PL", "LI", "HOA", "SINH", "TIN_HOC", "GIAO_DUC_CONG_DAN", "CN_CONG_NGHIEP", "CN_NONG_NGHIEP", "NGAY_SINH"]
+
+            for mon in mon_thi:
+                diem = diem_thi.get(mon, -1)
+                try:
+                    diem = float(diem)
+                    if diem >= 0: 
+                        output[mon] = diem
+                except:
+                    pass
+                
+            print(output)
+            return output
+
+        except Exception as e:
+            print('Không thể đọc dữ liệu'), str(e)
+
+for i in range(1000000, 1000100):
+    sbd = f"0{i}"
+    diem = tracudiem(sbd)
