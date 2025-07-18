c5e614f43c70c73781903ee9973fe03b8b07b26a
Hoàng Phúc <phuchn0305@gmail.com>
2025-07-18 15:10:20 +0700

    feat/def tracuudiem

---
+++ src/crawl_diem.py
@@ -0,0 +1,26 @@
+import requests
+
+def tracudiem (sbd: str, nam: int = 2025):
+    url = "https://s6.tuoitre.vn/api/diem-thi-thpt.htm"
+
+    # sbd = "01016799"
+
+    params = {
+        "sbd": sbd,
+        "year": nam
+    }
+
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115 Safari/537.36"
+    }
+
+    res = requests.get(url, params = params, headers = headers)
+
+    print('Status code: ', res.status_code)
+    try: 
+        data = res.text
+        print("Kết quả: ", data)
+    except: 
+        print("Không thể đọc json")
+
+tracudiem("01016799")
